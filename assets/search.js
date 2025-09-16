154ceb73e28debb771644499c34366e81003ca8c
zuqiuxiaojiang <zuqiuxiaojiang@users.noreply.github.com>
2025-09-16 10:48:00 +0000

    deploy: 30a1a6f049ff061b09e0f6d32b95a727ae680216

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/jekyll/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/jekyll/2018/07/01/welcome.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/jekyll/2018/07/01/welcome.html"},{'title':null,'url':"/jekyll/2025/09/16/2018-06-01-header-image.html"}]};
